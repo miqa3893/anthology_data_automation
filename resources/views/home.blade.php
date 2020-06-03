ce7e9be65874ace4cb259a09c7002dec724a9f10
miqa3893 <aquamikuln3893@gmail.com>
2020-06-04 00:20:45 +0900

    ログインしたヒTwitter名が表示されるように修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,6 +34,7 @@
             <div class="row">
                 <div class="col-2"></div>
                     <div class="col-8">
+                        <p class="lead">こんにちは、{{ $_GET['twitterName'] }}さん！</p>
                         <p class="lead">提出する作品の情報を入力してください。</p>
                         <br>
                         <form action="{{route('confirm')}}" method="post" enctype="multipart/form-data" class="needs-validation" novalidate>
